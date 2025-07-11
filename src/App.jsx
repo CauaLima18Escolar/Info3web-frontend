59c3e8c82277bbf1ae73d7a34e2b00aa29cc5295
Lucas de Lima Barros <20231144010027@ifrn.local>
2025-07-11 16:30:09 -0300

    Colocado o texto no centro da página

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <body className="flex flex-col items-center justify-center bg-body">
-      <div className="justify-center items-center mt-[220px] mr-[630px] bg-white text-center ">
+    <body className="h-screen flex items-center justify-center bg-body">
+      <div className="bg-white text-center ">
         <h1 className="text-home">Faça seu Login</h1>
       </div>
     </body>
