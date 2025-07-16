01c612681d652cd634e94e3dbc3ccd497cd678aa
Lucas <cucabarros1828@gmail.com>
2025-07-15 23:29:46 -0300

    Criadas as páginas de Login e Cadastro

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,18 +1,32 @@
 import "./index.css";
 import feijao from "./assets/feijao.svg";
 
-export default function App() {
+export default function Cadastro() {
   return (
     <body className="h-screen flex items-center justify-center bg-body">
       <div className="relative z-10 bg-white bg-opacity-80 py-8 px-4 rounded-lg text-center shadow-xl">
-        <div className="flex flex-col gap-y-[66px] px-10">
+        <div className="flex flex-col gap-y-[16px] px-10">
           <h1 className="text-roxo_destaque text-left text-[28px] font-bold border-b-3">
-            Faça seu Login
+            Faça seu cadastro
           </h1>
           <div className="flex flex-col gap-y-[8px]">
-            <h1 className="text-left font-medium">Matrícula:</h1>
+            <h1 className="text-left font-medium">Nome:</h1>
+            <input
+              placeholder="Insira seu nome"
+              className="w-[280px] px-2 py-1 rounded-lg border-1"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-y-[8px]">
+            <h1 className="text-left font-medium">Matricula:</h1>
             <input
               placeholder="Insira sua matrícula"
+              className="w-[280px] px-2 py-1 rounded-lg border-1"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-y-[8px]">
+            <h1 className="text-left font-medium">E-mail:</h1>
+            <input
+              placeholder="Insira seu e-mail"
               className="w-[280px] px-2 py-1 rounded-lg border-1"
             ></input>
           </div>
@@ -28,9 +42,9 @@ export default function App() {
               Enviar
             </button>
             <h1 className="flex items-center justify-center">
-              Não tem conta?
+              Já tem conta?
               <button className="px-1 cursor-pointer text-roxo_destaque border-b">
-                Faça seu cadastro
+                Faça login
               </button>
             </h1>
           </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Cadastro from './Cadastro.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />,
+    <Cadastro />,
 )
