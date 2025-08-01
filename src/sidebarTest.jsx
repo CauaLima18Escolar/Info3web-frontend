8e5b17db273c4a2491c436ef9a73bbdfd1d4f28c
Lucas de Lima Barros <20231144010027@ifrn.local>
2025-08-01 16:06:46 -0300

    continuação da criação do post

--- src/sidebarTest.jsx
+++ src/sidebarTest.jsx
@@ -1,14 +1,14 @@
-import Sidebar from "./components/sidebar.jsx";
-import Post from "./components/post.jsx"
+import Sidebar from "./components/Sidebar.jsx";
+import Post from "./components/Post.jsx"
 
 export default function SidebarTest() {
   return (
     <div className="flex bg-body">
       <Sidebar />
       <main className="pl-64 p-10">
-        <Post />
         <h1 className="text-2xl font-bold mb-4">Conteúdo ao lado da Sidebar</h1>
         <p>Testando layout com fundo</p>
+        <Post />
         <p className="py-200">Testando Sidebar fixa</p>
       </main>
     </div>
