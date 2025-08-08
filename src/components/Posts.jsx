65a826727d9901f7f5135b8e7e5f6edce1f48b39
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-08-08 14:39:35 -0300

    fix: Consertadas as imagens dos usuários na turma e consertado tamanho da lista da turma

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,23 +1,10 @@
-import imgPost1 from "../assets/Comunistas.svg";
-import imgPost2 from "../assets/Comunistas2.svg";
-import vinicius from "../assets/vini.svg";
 import Post from "./Post";
 import ApiService from "../service/ApiService";
 import { useEffect, useState } from "react";
 
-const postMocado = [
-  {
-    id: 1,
-    nomeUser: "Vini A.",
-    conteudo:
-      "Então, vocês sabiam que aqui no Brasil, durante o período de 1972 a 1975, aconteceu a Guerra da Araguaia? Basicamente foi um conflito envolvendo comunistas e o exército do Brasil, no meio da ditadura, e iss...",
-    imagensPost: [imgPost1, imgPost2],
-    imagemUser: vinicius,
-  },
-];
 
 export default function Posts() {
-  const [posts, setPosts] = useState(postMocado);
+  const [posts, setPosts] = useState([]);
   const { get } = ApiService;
 
   const handlePeriodicPosts = async () => {
--- src/pages/Turma.jsx
+++ src/pages/Turma.jsx
@@ -15,14 +15,16 @@ export default function Turma() {
     fetchAlunos();
   }, [get, usuarios]);
 
+
+
   return (
     <div className="flex flex-col justify-center items-center min-w-screen min-h-screen gap-y-5 bg-body p-10">
       <h1 className="text-roxo_destaque font-bold text-[48px]">Turma</h1>
-      <ul className="grid grid-cols-2 min-h-screen bg-white rounded-[10px] px-10 py-8 gap-x-20 gap-y-10">
+      <ul className="grid grid-cols-2 h-fit bg-white rounded-[10px] px-10 py-8 gap-x-20 gap-y-10">
         {usuarios.map((usuario) => (
           <li className="flex flex-col">
-            <div className="flex gap-x-4">
-              <p>img</p>
+            <div className="flex gap-x-4 items-center">
+              <p className="flex items-center justify-center bg-roxo_destaque text-white font-bold text-[16px] rounded-full p-1">{usuario.nome?.slice(0, 2).toUpperCase()}</p>
               <p className="text-[20px] font-bold">{usuario.nome}</p>
             </div>
             <p>{usuario.bio}</p>
